e8d0ee85e4e5473d4f8f812745c587016bd7963f
wpinrui <77185900+wpinrui@users.noreply.github.com>
2023-06-02 20:17:03 +0800

    Setup main.dart skeleton

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,26 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Random Velvet'),
+          backgroundColor: Colors.red[900],
+        ),
+        backgroundColor: Colors.redAccent,
+        body: RvPage(),
+      ),
+    );
+  }
+}
+
+class RvPage extends StatelessWidget {
+  const RvPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [],
+    );
   }
 }
